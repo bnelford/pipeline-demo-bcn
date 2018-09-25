1b3dad2e4dff0912f640e454e1754f7a25a91cc4
bnelford <beau.nelford@incontact.com>
2018-09-25 14:21:34 -0600

    Testing my tests

--- test/test.js
+++ test/test.js
@@ -14,7 +14,7 @@ describe('Run unit tests in memory',function() {
         request(server)
             .get('/marco')    
             .expect(200)
-            .expect('Polo!', done);
+            .expect('Mickey', done);
     });
     it('should 404 with anything else', function testPath(done){
         request(server)
